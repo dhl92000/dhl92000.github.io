c19578dbfcd47ebc31d3693aa2bba1e9e072f09b
dahee-lee <dahlee97@gmail.com>
2024-05-23 23:50:59 -0400

    Portfolio Redesign

---
+++ src/components/Story.js
@@ -0,0 +1,39 @@
+import lisbon from '../projectData/lisbon.png'
+import '../pages/AboutPage'
+
+const Story = () => {
+    return (
+        <div class="storyDiv">
+            <h1>My Story<span style={{ color: "#0E8D44", fontSize: "1.5em" }}>.</span></h1>
+            <div class="story">
+                <div class="storyText">
+                    <p>
+                        Hi, I'm Dahee!
+                    </p>
+                    <p>
+                        My career journey has not been straightforward. I started my career in the aviation industry and
+                        became addicted to travel and exploring new places. During this time, I developed a knack for
+                        connecting with customers, and an interest in the Toronto real estate market. I started my
+                        business as a real estate salesperson, growing a repeat clientele through referrals, creating a
+                        marketing plan and website, and everything in between.
+                    </p>
+
+                    <p>
+                        While in these industries, I noticed that my clients had a lot of trouble using applications
+                        with hyper-minimal user interfaces. Similarly, at the airport, I would witness passengers
+                        staring blankly at the self-check-in terminals, not knowing what to do.
+                    </p>
+                    <p>
+                        This curiosity was what started my journey in software development. I am a full stack developer and am passionate about creating responsive, intuitive interfaces and scalable applications. Feel free to connect with me on LinkedIn or reach out at dahlee97@gmail.com.
+                    </p>
+                </div>
+
+                <div id="storyImageDiv">
+                    <img src={lisbon} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Story
---
+++ src/pages/Homepage.js
@@ -0,0 +1,104 @@
+import './Homepage.css'
+import Projects from '../components/Projects';
+import IMG_1290 from '../projectData/IMG_1290.jpg'
+import allProjects from '../projectData/projectData'
+
+const Homepage = () => {
+
+    return (
+        <div class="homepage">
+            <div class="intro">
+                {/* intro */}
+                <div class="introTextDiv">
+                    <p class="introName">Dahee Lee</p>
+
+                    <p id="nameDef"><span style={{ color: "#0E8D44", fontSize: "1.5em" }}>&#10059;</span> / dahi / <span
+                        id="nameKorean"> 다희 </span></p>
+
+                    <div class="introBlurb">
+                        <p>
+                            Creative fullstack software engineer obsessed with clean designs and clean code.
+                        </p>
+
+                        <p>
+                            My passion is creating responsive, inspiring websites and applications that are intuitive for users. With an entrepreneurial background and excellent communication skills, I am adept at balancing both business and product needs when developing.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="introImageDiv">
+                    <img class="introImage" src={IMG_1290} alt="Profile picture of Dahee" />
+                </div>
+            </div>
+            {/* skills */}
+            <div class="skillsSection">
+                <div>
+                    <h1>Skills<span style={{ color: "#0E8D44", fontSize: "1.5em" }}>.</span> </h1>
+                </div>
+
+                <div class="skills">
+                    <div class="skillsDiv">
+                        <p>
+                            Languages
+                        </p>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
+                    <div class="skillsDiv">
+                        <p>
+                            Libraries, frameworks
+                        </p>
+                        <ul>
+                            <li>ExpressJS</li>
+                            <li>Django</li>
+                            <li>React</li>
+                            <li>Tailwind CSS</li>
+                        </ul>
+                    </div>
+
+                    <div class="skillsDiv">
+                        <p>
+                            Technologies
+                        </p>
+
+                        <ul>
+                            <li>AWS</li>
+                            <li>Github</li>
+                            <li>Heroku</li>
+                            <li>MongoDB</li>
+                            <li>MySQL</li>
+                            <li>Netlify</li>
+                            <li>PostgreSQL</li>
+                        </ul>
+                    </div>
+
+                    <div class="skillsDiv">
+                        <p>
+                            Soft Skills
+                        </p>
+                        <ul>
+                            <li>Collaboration</li>
+                            <li>Conflict-resolution</li>
+                            <li>Effective Communication</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            {/* projects */}
+            <Projects allProjects={allProjects} />
+
+        </div>
+
+    )
+}
+
+export default Homepage
