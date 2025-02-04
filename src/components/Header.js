653179510612dc176f548249d42d7c6eb78d7507
dahee-lee <dahlee97@gmail.com>
2025-02-04 16:49:19 -0500

    Add animation to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,56 @@
-import './Header.css';
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { useScroll, motion, useMotionValueEvent } from "motion/react";
 
 const Header = () => {
-    return (
-        <header class="header">
-            <div id="headerName">
-                <Link to="/">( DAHEE LEE )</Link>
-            </div>
-            <div class="headerNav">
-            <Link to="/">
-                    <button class="keyboardButton">
-                        Home
-                    </button>
-                </Link>
+  const [isHidden, setIsHidden] = useState(false);
 
-                <Link to="/about">
-                    <button class="keyboardButton">
-                        About
-                    </button>
-                </Link>
-                <Link to="/contact">
-                    <button class="keyboardButton">
-                        Contact
-                    </button>
-                </Link>
-            </div>
-        </header>
-    )
-}
+  // # of pixels user is scrolled down on page
+  const { scrollY } = useScroll();
+  const lastYRef = useRef(0);
+  //react to something that changes in scrollY
+  useMotionValueEvent(scrollY, "change", (y) => {
+    const difference = y - lastYRef.current;
+    if (Math.abs(difference) > 50) {
+      setIsHidden(difference > 0);
+      lastYRef.current = y;
+    }
+  });
 
-export default Header
+  return (
+    <motion.header
+      class="header"
+      animate={isHidden ? "hidden" : "visible"}
+      whileHover={"visible"}
+      onFocusCapture={() => setIsHidden(false)}
+      variants={{
+        hidden: {
+          y: "-75%",
+        },
+        visible: {
+          y: "0%",
+        },
+      }}
+      transition={{ duration: 0.2 }}
+    >
+      <div id="headerName">
+        <Link to="/">( DAHEE LEE )</Link>
+      </div>
+      <div class="headerNav">
+        <Link to="/">
+          <button class="keyboardButton">Home</button>
+        </Link>
+
+        <Link to="/about">
+          <button class="keyboardButton">About</button>
+        </Link>
+        <Link to="/contact">
+          <button class="keyboardButton">Contact</button>
+        </Link>
+      </div>
+    </motion.header>
+  );
+};
+
+export default Header;
